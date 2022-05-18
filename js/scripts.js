e033fc84ee2071efa3094d69c7095aec8c0b8e01
vapx <g.edmon25@gmail.com>
2022-05-18 19:09:38 +0800

    refactor: configure smtp email for e-lingkod

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,12 @@
 
 const sendEmail = () => {
   Email.send({
-    secureToken:
-      'F0CF9998DAB3225D382F0F6C368FE622D91624DE9AB88E836EC6CA2075A1765E98BD202EB6F8D4C31176725DED7A6088',
     Host: 'smtp.elasticemail.com',
     Username: 'g.edmon25@gmail.com',
     Password: 'AAA4EE3F5D68454F8C0937816DE182F78254',
     To: 'garciaedmon025@gmail.com',
-    From: document.getElementById('emailAddressBelow').value,
-    Subject: 'Additional User',
-    Body: 'We have a new user'
+    From: document.querySelector('.e-lingkod-form-input').value,
+    Subject: document.querySelector('.e-lingkod-form-input').value,
+    Body: document.querySelector('.e-lingkod-form-input').value
   }).then((message) => alert(message))
 }
